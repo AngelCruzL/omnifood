5a78e04dba5500e11db79e772bf7785485d17ebf
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-08 13:36:36 -0500

    feat: adding smooth scrolling animation

--- js/script.js
+++ js/script.js
@@ -8,6 +8,31 @@ yearEl.textContent = currentYear;
 const btnNavEl = document.querySelector('.btn-mobile-nav');
 const headerEl = document.querySelector('.header');
 
-btnNavEl.addEventListener('click', function () {
+btnNavEl.addEventListener('click', () => {
   headerEl.classList.toggle('nav-open');
+});
+
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
 });
