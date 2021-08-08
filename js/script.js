c5f75f62803adf46be98fd58f5f0b7672ef15479
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-08 13:22:37 -0500

    feat: implementing functionality on mobile navigation

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Mobile navigation
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
